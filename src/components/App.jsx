9c51ffc8c9e394158af6d65406e9a0b2c0159de3
Spec27 <shkvara27@gmail.com>
2022-06-19 00:15:07 +0300

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
-export const App = () => {
-  return (
-    <div
+import React,{Component} from "react";
+
+class App extends Component{
+  render() {
+    return (
+      <div
       style={{
         height: '100vh',
         display: 'flex',
@@ -10,7 +13,10 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      React homework template gggggg
     </div>
-  );
-};
+    )
+  }
+}
+export default App 
+ 
